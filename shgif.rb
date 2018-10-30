9772a7364c32b83675f6cfb959060c97a253b2cf
Cj-bc <cj-bc@y-modify.org>
2018-10-30 18:54:37 +0900

    Add: v1.2.0

---
+++ shgif.rb
@@ -0,0 +1,17 @@
+class Shgif < Formula
+  desc "a Shell GIF animation viewer"
+  homepage "https://github.com/Cj-bc/shgif.git"
+  version "1.2.0"
+  url "https://github.com/Cj-bc/shgif/archive/#{version}.tar.gz"
+  sha256 "93dd25abb2a3294295e6a3e8c6cd6772dfe45f32604b2603b3a2992d81ea27c2"
+
+  def install
+    system "mv", "shgif.sh", "shgif"
+    bin.install "shgif"
+    prefix.install Dir["lib"], Dir["deps"], Dir["tests"]
+  end
+
+  test do
+    system "bats", "#{prefix}/tests/*"
+  end
+end
