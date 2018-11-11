c32234801ddadc3f86889e36233586dd1887cb69
Cj-bc <cj-bc@y-modify.org>
2018-11-11 22:46:32 +0900

    Update: v1.4.2

--- shgif.rb
+++ shgif.rb
@@ -1,9 +1,9 @@
 class Shgif < Formula
   desc "a Shell GIF animation viewer"
   homepage "https://github.com/Cj-bc/shgif.git"
-  version "1.4.1"
+  version "1.4.2"
   url "https://github.com/Cj-bc/shgif/archive/v#{version}.tar.gz"
-  sha256 "2da9291603cfc856b731cc972642b9d111a8833e63e56a549edb1d35e963027b"
+  sha256 "c21667996929a391640d4319a9eb19138b1ae932ab7f5aa0a7bb7aa6009848f0"
 
   def install
     system 'vim +\'%s#\$( cd "\${BASH_SOURCE\[0\]%\/\*}" && pwd )#' + prefix + '#g\' +w! +q shgif.sh'
