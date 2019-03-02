5e101a802398d4b7186be0b852badd0f3d51cd86
Cj-bc <cj-bc@y-modify.org>
2019-03-02 15:35:24 +0900

    Update: 1.5.0

--- shgif.rb
+++ shgif.rb
@@ -1,15 +1,19 @@
 class Shgif < Formula
   desc "a Shell GIF animation viewer"
   homepage "https://github.com/Cj-bc/shgif.git"
-  version "1.4.3"
+  version "1.5.0"
   url "https://github.com/Cj-bc/shgif/archive/v#{version}.tar.gz"
-  sha256 "da49bfb947293ac74ddec3464c032c81de9129a005c57927b42f48531445645f"
+  sha256 "8a9026835c2c0d18fe490fba65c4f1d27e75e954d259f901957df88a404c15b9"
+
+  depends_on "Cj-bc/blib"
 
   def install
-    system 'vim +\'%s#\$( cd "\${BASH_SOURCE\[0\]%\/\*}" && pwd )#' + prefix + '#g\' +w! +q shgif.sh'
     system "mv", "shgif.sh", "shgif"
     bin.install "shgif"
-    prefix.install Dir["lib"], Dir["deps"], Dir["tests"]
+    prefix.install Dir["lib"], Dir["tests"]
+
+    system "blib", "install", "'Cj-bc/libtar'"
+    system "blib", "install", "'Cj-bc/libfile'"
   end
 
   test do
