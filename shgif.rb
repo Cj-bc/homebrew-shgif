d69e3d08ad53c16d275127efcc00c6723a0e4bfb
Cj-bc <cj-bc@y-modify.org>
2019-03-03 16:15:42 +0900

    Update: v1.5.1

--- shgif.rb
+++ shgif.rb
@@ -1,22 +1,23 @@
 class Shgif < Formula
   desc "a Shell GIF animation viewer"
   homepage "https://github.com/Cj-bc/shgif.git"
-  version "1.5.0"
+  version "1.5.1"
   url "https://github.com/Cj-bc/shgif/archive/v#{version}.tar.gz"
-  sha256 "8a9026835c2c0d18fe490fba65c4f1d27e75e954d259f901957df88a404c15b9"
+  sha256 "6191eafc607b6780c81db7ee94eccfd20e80d437b2600ae0b813c6f308d9a78c"
 
   depends_on "cj-bc/blib/blib"
 
   def install
     system "mv", "shgif.sh", "shgif"
     bin.install "shgif"
-    prefix.install Dir["lib"], Dir["tests"]
+    prefix.install Dir["tests"]
   end
 
   def caveats; <<~EOS
     Please install dependencies by:
-      `blib install Cj-bc/libtar`
-      `blib install Cj-bc/libfile`
+      $ blib install Cj-bc/libtar
+      $ blib install Cj-bc/libfile
+      $ blib install Cj-bc/shgif
   EOS
   end
 
