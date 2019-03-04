5de6603c02f6913bf4efb96d6a979d5e6d48bcd0
Cj-bc <cj-bc@y-modify.org>
2019-03-04 11:44:10 +0900

    update: 1.5.2

--- shgif.rb
+++ shgif.rb
@@ -1,9 +1,9 @@
 class Shgif < Formula
   desc "a Shell GIF animation viewer"
   homepage "https://github.com/Cj-bc/shgif.git"
-  version "1.5.1"
+  version "1.5.2"
   url "https://github.com/Cj-bc/shgif/archive/v#{version}.tar.gz"
-  sha256 "6191eafc607b6780c81db7ee94eccfd20e80d437b2600ae0b813c6f308d9a78c"
+  sha256 "892cf4d4e654768c797b270e85058f2dcd9b4bb4bd392108de104ad248dd0305"
 
   depends_on "cj-bc/blib/blib"
 
